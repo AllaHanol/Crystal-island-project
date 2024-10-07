ef7e971695cae04da78d5195fb295d7e4001a700
grizeus <zaytsewww@gmail.com>
2024-10-07 21:55:37 +0300

    simple gallery adjust

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,56 +1,10 @@
-// let offset = 0; //зміщення від лівого краю
-// const gallery = document.querySelector('.gallery');
-
-// document
-//   .querySelector('.gallery-next-btn')
-//   .addEventListener('click', function () {
-//     offset += 270;
-//     if (offset > 1200) {
-//       offset = 0;
-//     }
-//     gallery.style.left = -offset + 'px';
-//   });
-// document
-//   .querySelector('.gallery-prev-btn')
-//   .addEventListener('click', function () {
-//     offset -= 270;
-//     if (offset < 0) {
-//       offset = 1200;
-//     }
-//     gallery.style.left = -offset + 'px';
-//   });
-
-// import Swiper from 'swiper/bundle';
-// import 'swiper/scss';
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const width = window.innerWidth;
-//   let slidesPerView;
-//   if (width > 320 && width < 1440) {
-//     slidesPerView = 2;
-//   } else {
-//     slidesPerView = 1;
-//   }
-//   new Swiper('.swiper-reviews', {
-//     slidesPerView,
-//     spaceBetween: 0,
-//     slideToClickedSlide: true,
-//     loop: true,
-//     loopedSlides: 1,
-//     keyboard: {
-//       enabled: true,
-//       onlyInViewport: true,
-//     },
-//   });
-// });
-
 'use strict';
 
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 export default (() => {
-  const swiper = new Swiper('.gallery-container', {
+  const swiper = new Swiper('.gallery-slider', {
     slidesPerView: 'auto',
     spaceBetween: 24,
 
@@ -64,6 +18,11 @@ export default (() => {
       type: 'bullets',
       el: '.gallery-controls',
       clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
 })();
