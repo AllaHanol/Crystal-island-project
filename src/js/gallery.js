5da4da3a4441a6a4801e95b54f4eb2f66962c278
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-10-07 20:03:19 +0300

    gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,45 +1,69 @@
-let offset = 0; //зміщення від лівого краю
-const gallery = document.querySelector('.gallery');
+// let offset = 0; //зміщення від лівого краю
+// const gallery = document.querySelector('.gallery');
 
-document
-  .querySelector('.gallery-next-btn')
-  .addEventListener('click', function () {
-    offset += 270;
-    if (offset > 1200) {
-      offset = 0;
-    }
-    gallery.style.left = -offset + 'px';
-  });
-document
-  .querySelector('.gallery-prev-btn')
-  .addEventListener('click', function () {
-    offset -= 270;
-    if (offset < 0) {
-      offset = 1200;
-    }
-    gallery.style.left = -offset + 'px';
-  });
+// document
+//   .querySelector('.gallery-next-btn')
+//   .addEventListener('click', function () {
+//     offset += 270;
+//     if (offset > 1200) {
+//       offset = 0;
+//     }
+//     gallery.style.left = -offset + 'px';
+//   });
+// document
+//   .querySelector('.gallery-prev-btn')
+//   .addEventListener('click', function () {
+//     offset -= 270;
+//     if (offset < 0) {
+//       offset = 1200;
+//     }
+//     gallery.style.left = -offset + 'px';
+//   });
+
+// import Swiper from 'swiper/bundle';
+// import 'swiper/scss';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const width = window.innerWidth;
+//   let slidesPerView;
+//   if (width > 320 && width < 1440) {
+//     slidesPerView = 2;
+//   } else {
+//     slidesPerView = 1;
+//   }
+//   new Swiper('.swiper-reviews', {
+//     slidesPerView,
+//     spaceBetween: 0,
+//     slideToClickedSlide: true,
+//     loop: true,
+//     loopedSlides: 1,
+//     keyboard: {
+//       enabled: true,
+//       onlyInViewport: true,
+//     },
+//   });
+// });
+
+'use strict';
 
 import Swiper from 'swiper/bundle';
-import 'swiper/scss';
+import 'swiper/css/bundle';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const width = window.innerWidth;
-  let slidesPerView;
-  if (width > 320 && width < 1440) {
-    slidesPerView = 2;
-  } else {
-    slidesPerView = 1;
-  }
-  new Swiper('.swiper-reviews', {
-    slidesPerView,
-    spaceBetween: 0,
-    slideToClickedSlide: true,
-    loop: true,
-    loopedSlides: 1,
+export default (() => {
+  const swiper = new Swiper('.gallery-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+
     keyboard: {
       enabled: true,
       onlyInViewport: true,
+      pageUpDown: true,
+    },
+
+    pagination: {
+      type: 'bullets',
+      el: '.gallery-controls',
+      clickable: true,
     },
   });
-});
+})();
