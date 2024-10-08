0f58ab01d674de58ffc4445881fb62af94ccbff8
grizeus <zaytsewww@gmail.com>
2024-10-08 13:33:08 +0300

    fix typo in doc name, remove slider.html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // імпортувати файли із папки js , в яких обробили функціонал секціі
 import './js/header';
-import './js/advanatages.js';
+import './js/advantages.js';
 import './js/reviews.js';
 import './js/gallery.js';
